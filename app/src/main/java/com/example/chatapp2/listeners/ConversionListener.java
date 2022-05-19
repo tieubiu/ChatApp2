1ceedc442d64b21e39962df61479a5c1fbc7d39d
Admin <Admin@ADMIN>
2022-05-19 11:37:40 +0700

    Add project files.

---
+++ app/src/main/java/com/example/chatapp2/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.example.chatapp2.listeners;
+
+import com.example.chatapp2.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/example/chatapp2/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.example.chatapp2.listeners;
+
+import com.example.chatapp2.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
